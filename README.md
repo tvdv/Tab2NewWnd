1f417c84d20e5d27e54358d3b29942c23070152f
tvdv <tvdv@server.fake>
2014-04-19 09:01:49 +1000

    tidying up code and readme

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 Tab2NewWnd
 ==========
 
-Chrome extension that moves a tab into its own window
+Chrome extension that moves a tab into its own window. You can bind a shortcut key to this action, allowing you to put a tab in a new window, without taking your hands off the keyboard.
+
 
 To load the extension, as an unpack directory, see https://developer.chrome.com/extensions/getstarted.html.
-To create a distributable package, see http://developer.chrome.com/extensions/packaging.html
+To create a distributable package, see https://developer.chrome.com/extensions/packaging.html
+
+
+Icon is public domain. Source: http://thenounproject.com/term/floating-window/2308/
--- TabToNewWnd/background.js
+++ TabToNewWnd/background.js
@@ -1,30 +1,20 @@
-
+//Copyright (c) 2013-2014, Tvdv
 
 var currentTab=null;
 function moveTabClick(tab) {
-  //chrome.tabs.getCurrent(getCurrentTabCB);
   chrome.tabs.query({ currentWindow: true, active: true }, getCurrentTabCB);
 }
 
 function getCurrentTabCB(tabs) {
-	if (typeof tabs === "undefined") 
-	{
+	if (typeof tabs === "undefined") {
    		console.error("not a valid tab");
-   	}
-   	else
-   	{
+   	} else {
 		currentTab=tabs[0];	
-
 		//create a new wnd and put the tab in it
 		chrome.windows.create({"tabId":currentTab.id},windowCreateCB);
 	}
 }
-function windowCreateCB(win)
-{
-	console.log("window created callback");
-
+function windowCreateCB(win) {
 }
 
-
-console.log("register click listener");
 chrome.browserAction.onClicked.addListener(moveTabClick);
